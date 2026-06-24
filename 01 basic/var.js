e7a5653d728fa06fc0857be7c434579ef9ed0b4a
myselfchandrakantmandal-creator <myself.chandrakantmandal@gmail.com>
2026-06-24 12:39:11 +0000

    datatype

---
+++ 01 basic/var.js
@@ -0,0 +1,24 @@
+const accountId =144567
+let email ="chandrakant67@gmail.com"
+var pas ="12345"
+console.log(accountId);
+console.table([accountId,email,pas]);
+
+"use strict";//treat all js code as newer version
+// we are using node not browser alert(3+3)
+console.log(3+6);
+
+
+//data types
+let name="chandrakant"
+let age=17
+let login =true
+//bigint
+//number
+//string""
+// boolean true ya false
+//null stand alone value empty value
+//undefined=not defined a value 
+//symbol = unique
+//object
+console.log(typeof age)
